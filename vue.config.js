95ec5f4ba7f7d54b6e598306cc3645d568155e5d
mgiesen <maximilian.giesen@sls.siempelkamp.com>
2024-02-11 21:25:28 +0100

    Changed publicPath to match GitHub Pages root

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Example-Vue-Dashboard/'
+    : '/'
 });
